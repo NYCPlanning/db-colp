9479011686bcb9eadad9492d4aeaf25abcb57b97
mgraber <molly.graber@me.com>
2020-03-23 19:12:19 +0000

    Add residential mapping from use codes

--- colp_build/02_build.sh
+++ colp_build/02_build.sh
@@ -6,6 +6,7 @@ START=$(date +%s);
 # Column mapping
 psql $BUILD_ENGINE -f sql/create.sql
 psql $BUILD_ENGINE -f sql/map_ipis.sql
+psql $BUILD_ENGINE -f sql/residential_occ.sql
 
 END=$(date +%s);
 echo $((END-START)) | awk '{print int($1/60)" minutes and "int($1%60)" seconds elapsed."}'

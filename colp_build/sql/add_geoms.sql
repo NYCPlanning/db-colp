649017559456ccbff75bc73a658b554b07283299
mgraber <molly.graber@me.com>
2020-03-26 12:33:07 +0000

    Add hnum and street from geocoding results

--- colp_build/sql/add_geoms.sql
+++ colp_build/sql/add_geoms.sql
@@ -2,6 +2,8 @@
 
 UPDATE colp
 SET x_coord = a.x_coord,
-    y_coord = a.y_coord
+    y_coord = a.y_coord,
+    hnum = a.hnum,
+    street = a.sname
 FROM dcas_ipis_geocodes a
 WHERE colp.bbl = a.input_bbl;

8ae31c7f9378a16a48b60060f27f9d4db409b4b3
mgraber <molly.graber@me.com>
2020-04-08 12:48:00 +0000

    Remove no_curr_use and residential_occ

--- colp_build/02_build.sh
+++ colp_build/02_build.sh
@@ -7,8 +7,6 @@ START=$(date +%s);
 psql $BUILD_ENGINE -f sql/create.sql
 psql $BUILD_ENGINE -f sql/map_ipis.sql
 psql $BUILD_ENGINE -f sql/add_geoms.sql
-psql $BUILD_ENGINE -f sql/residential_occ.sql
-psql $BUILD_ENGINE -f sql/no_curr_use.sql
 psql $BUILD_ENGINE -f sql/cat_codes.sql
 
 END=$(date +%s);
--- colp_build/sql/residential_occ.sql
+++
@@ -1,7 +0,0 @@
--- Derive residential use from primary use
-UPDATE colp
-SET residential_occ = CASE
-        WHEN use_code = '1410' THEN 'O'
-        WHEN use_code = '1420' THEN 'N'
-        ELSE NULL
-    END;

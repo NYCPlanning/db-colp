b9886f8db5057075bbc54cd570abc42f0f5f6356
mgraber <molly.graber@me.com>
2020-04-17 19:20:02 +0000

    Account for address then BBL geocoding

--- colp_build/sql/add_geoms.sql
+++ colp_build/sql/add_geoms.sql
@@ -6,4 +6,6 @@ SET x_coord = a.x_coord,
     hnum = a.hnum,
     street = a.sname
 FROM dcas_ipis_geocodes a
-WHERE colp.bbl = a.input_bbl;
+WHERE colp.bbl = a.bbl
+AND colp.hnum = a.house_number
+AND colp.street = a.street_name;

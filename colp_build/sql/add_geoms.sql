2d454f47bc2f527b5023b46a269b63ea322caa2a
mgraber <molly.graber@me.com>
2020-04-21 12:25:36 +0000

    Populate mappable and add geom field

--- colp_build/sql/add_geoms.sql
+++ colp_build/sql/add_geoms.sql
@@ -1,5 +1,4 @@
--- Add geometries from geocoding results
-
+-- Add coordinates from geocoding results
 UPDATE colp
 SET x_coord = a.x_coord,
     y_coord = a.y_coord,
@@ -7,3 +6,17 @@ SET x_coord = a.x_coord,
     street = a.sname
 FROM dcas_ipis_geocodes a
 WHERE colp.bbl = a.input_bbl;
+
+-- Create geom column from coordinates
+UPDATE colp
+SET geom = ST_SetSRID(ST_MakePoint(a.longitude::double precision, a.latitude::double precision),4326)
+FROM dcas_ipis_geocodes a
+WHERE colp.bbl = a.input_bbl
+AND a.longitude IS NOT NULL AND a.longitude <> '';
+
+-- Set mappable flag to indicate missing geometries
+UPDATE colp
+SET mappable = (CASE 
+    WHEN x_coord IS NULL THEN 1
+    ELSE 0
+    END);

e7c452321df3d49ea6c62e7d590a72e74323ed5c
mgraber <molly.graber@me.com>
2020-08-17 15:33:50 +0000

    Remove unnecessary env from geocoding docker

--- colp_build/02_build.sh
+++ colp_build/02_build.sh
@@ -6,7 +6,6 @@ START=$(date +%s);
 docker run --rm\
     -v `pwd`:/home/colp_build\
     -w /home/colp_build\
-    -e RECIPE_ENGINE=$RECIPE_ENGINE\
     -e BUILD_ENGINE=$BUILD_ENGINE\
     nycplanning/docker-geosupport:latest bash -c "python3 python/geocode.py"
 

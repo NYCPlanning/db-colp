f067e39a506197a0247eef2ca66ab14e20b89666
mgraber <mgraber@planning.nyc.gov>
2021-03-11 15:56:16 +0000

    Export modified hnums

--- colp_build/sql/_export.sql
+++ colp_build/sql/_export.sql
@@ -59,3 +59,16 @@ FROM dcas_ipis a
 JOIN dcas_ipis_geocodes b
 ON a.bbl = b.input_bbl
 AND md5(CAST((a.*)AS text)) IN (SELECT DISTINCT dcas_ipis_uid FROM _colp WHERE "XCOORD" IS NULL);
+
+DROP TABLE IF EXISTS modified_hnums;
+SELECT 
+    dcas_bbl, 
+    dcas_hnum, 
+    display_hnum, 
+    dcas_sname, 
+    sname_1b
+INTO modified_hnums
+FROM ipis_colp_geoerrors
+WHERE dcas_hnum <> display_hnum
+OR (dcas_hnum IS NOT NULL AND display_hnum = '')
+OR (dcas_hnum IS NULL AND display_hnum <> '');

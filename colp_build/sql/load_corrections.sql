b18231a6bcda32bb736abcb86248d346a2194561
mgraber <mgraber@planning.nyc.gov>
2021-05-03 17:36:03 +0000

    Add reviewed_modified_names workflow

--- colp_build/sql/load_corrections.sql
+++ colp_build/sql/load_corrections.sql
@@ -10,3 +10,20 @@ CREATE TABLE corrections(
 );
 
 \COPY corrections FROM '_data/corrections.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS reviewed_modified_names;
+CREATE TABLE reviewed_modified_names(
+    dcas_bbl text,
+    dcas_hnum text,
+    display_hnum character varying(20),
+    dcas_sname text,
+    sname_1b text,
+    parcel_name character varying,
+    display_name text,
+    agency character varying,
+    primary_use_code character varying,
+    primary_use_text character varying,
+    reviewed text
+);
+
+\COPY reviewed_modified_names FROM '_data/ipis_modified_names.csv' DELIMITER ',' CSV HEADER;

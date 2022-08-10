1f1484df0e86aeb3c47b3edd653f778c5f259301
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-08-10 14:38:39 -0400

    split QAQC it separate bash script

---
+++ bash/03_qaqc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source bash/config.sh
+
+echo "Running QAQC"
+docker run --rm\
+    --network host\
+    -v `pwd`:/home/colp_build/\
+    -w /home/colp_build\
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    -e BUILD_ENGINE=$BUILD_ENGINE\
+    nycplanning/docker-geosupport:latest bash -c "python3 -m python.geo_qaqc"
+
+psql $BUILD_ENGINE -f sql/geo_qaqc.sql
+psql $BUILD_ENGINE -f sql/export_qaqc.sql

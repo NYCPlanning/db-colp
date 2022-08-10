59cdcfc0f001901f85be4a9cea70c51b3b328bfe
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-08-10 15:04:55 -0400

    send qaqc files to separate folder

--- bash/03_export.sh
+++ bash/03_export.sh
@@ -10,6 +10,14 @@ mkdir -p output && (
     SHP_export colp POINT
     FGDB_export colp POINT
 
+    echo "[$(date)] $DATE" > version.txt
+
+
+)
+
+mkdir -p output/qaqc && (
+    cd output/qaqc
+    echo "exporting qaqc"
     CSV_export ipis_unmapped
     CSV_export ipis_colp_geoerrors
     CSV_export ipis_sname_errors
@@ -21,9 +29,8 @@ mkdir -p output && (
     CSV_export usetype_changes
     CSV_export modifications_applied
     CSV_export modifications_not_applied
-    echo "[$(date)] $DATE" > version.txt
-
 )
+
 
 zip -r output/output.zip output
 

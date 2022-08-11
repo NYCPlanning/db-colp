7133121b618f57bd1f574e416ee67028f8a165d4
Te Du <tedudevelop@gmail.com>
2022-08-11 15:38:29 -0400

    uncomment build

--- bash/02_build.sh
+++ bash/02_build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 source bash/config.sh
 
-# psql $BUILD_ENGINE -f sql/load_modifications.sql  
+psql $BUILD_ENGINE -f sql/load_modifications.sql  
 
-# psql $BUILD_ENGINE -f sql/geo_inputs.sql
+psql $BUILD_ENGINE -f sql/geo_inputs.sql
 
-# docker run --rm\
-#     --network host\
-#     -v `pwd`:/home/colp_build/\
-#     -w /home/colp_build\
-#     -u $(id -u ${USER}):$(id -g ${USER}) \
-#     -e BUILD_ENGINE=$BUILD_ENGINE\
-#     nycplanning/docker-geosupport:latest bash -c "python3 -m python.geo_qaqc;
-#                                                 python3 -m python.geocode"
+docker run --rm\
+    --network host\
+    -v `pwd`:/home/colp_build/\
+    -w /home/colp_build\
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    -e BUILD_ENGINE=$BUILD_ENGINE\
+    nycplanning/docker-geosupport:latest bash -c "python3 -m python.geo_qaqc;
+                                                python3 -m python.geocode"
 
-# psql $BUILD_ENGINE -f sql/_procedures.sql
-# psql $BUILD_ENGINE -f sql/clean_parcelname.sql
-# psql $BUILD_ENGINE -f sql/create_colp.sql
-# psql $BUILD_ENGINE -f sql/geo_qaqc.sql
+psql $BUILD_ENGINE -f sql/_procedures.sql
+psql $BUILD_ENGINE -f sql/clean_parcelname.sql
+psql $BUILD_ENGINE -f sql/create_colp.sql
+psql $BUILD_ENGINE -f sql/geo_qaqc.sql
 
-# psql $BUILD_ENGINE -1 -c "CALL apply_correction('_colp', 'modifications');"
+psql $BUILD_ENGINE -1 -c "CALL apply_correction('_colp', 'modifications');"
 
 psql $BUILD_ENGINE -f sql/qc_geospatial_check.sql 
 
